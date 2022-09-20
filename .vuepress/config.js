eccc925d860b940694e6c135bfb27d2935c176e8
Joe Appleton <joe.appleton@solent.ac.uk>
2022-09-20 12:44:18 +0100

    bot commit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: "/advanced-database-systems-2021/",
+  base: "/advanced-database-systems-2022/",
   dest: "docs",
   markdown: {
     lineNumbers: true,
@@ -27,56 +27,56 @@ module.exports = {
         children: ["sessions/week_1/lecture", "/sessions/week_1/"],
       },
 
-      {
-        title: "Week 2 - Exploring Node, Express and Templates",
-        collapsable: true,
-        children: ["sessions/week_2/lecture", "/sessions/week_2/"],
-      },
-      {
-        title: "Week 3 - No Sql data planning and Modelling",
-        collapsable: true,
-        children: ["sessions/week_3/lecture", "/sessions/week_3/"],
-      },
-      {
-        title: "Week 4  - Completing the MVC stack",
-        collapsable: true,
-        children: ["/sessions/week_4/lecture", "/sessions/week_4/"],
-      },
+      // {
+      //   title: "Week 2 - Exploring Node, Express and Templates",
+      //   collapsable: true,
+      //   children: ["sessions/week_2/lecture", "/sessions/week_2/"],
+      // },
+      // {
+      //   title: "Week 3 - No Sql data planning and Modelling",
+      //   collapsable: true,
+      //   children: ["sessions/week_3/lecture", "/sessions/week_3/"],
+      // },
+      // {
+      //   title: "Week 4  - Completing the MVC stack",
+      //   collapsable: true,
+      //   children: ["/sessions/week_4/lecture", "/sessions/week_4/"],
+      // },
 
-      {
-        title: "Week 5 - Getting to Grips With the Assessment",
-        collapsable: true,
-        children: ["/sessions/week_5/"],
-      },
+      // {
+      //   title: "Week 5 - Getting to Grips With the Assessment",
+      //   collapsable: true,
+      //   children: ["/sessions/week_5/"],
+      // },
 
-      {
-        title: "Week 6 - Thinking About Updating Data",
-        collapsable: true,
-        children: ["/sessions/week_6_a/lecture", "/sessions/week_6_a/"],
-      },
+      // {
+      //   title: "Week 6 - Thinking About Updating Data",
+      //   collapsable: true,
+      //   children: ["/sessions/week_6_a/lecture", "/sessions/week_6_a/"],
+      // },
 
-      {
-        title: "Week 7 - Data Modeling and Relationships",
-        collapsable: true,
-        children: ["/sessions/week_7/lecture", "/sessions/week_7/"],
-      },
-      {
-        title: "Week 8 - User Authentication",
-        collapsable: true,
-        children: ["/sessions/week_8/lecture", "/sessions/week_8/"],
-      },
+      // {
+      //   title: "Week 7 - Data Modeling and Relationships",
+      //   collapsable: true,
+      //   children: ["/sessions/week_7/lecture", "/sessions/week_7/"],
+      // },
+      // {
+      //   title: "Week 8 - User Authentication",
+      //   collapsable: true,
+      //   children: ["/sessions/week_8/lecture", "/sessions/week_8/"],
+      // },
 
-      {
-        title: "Week 9 - Deploying to a Serverless Infrastructure",
-        collapsable: true,
-        children: ["/sessions/week_9/lecture", "/sessions/week_9/"],
-      },
+      // {
+      //   title: "Week 9 - Deploying to a Serverless Infrastructure",
+      //   collapsable: true,
+      //   children: ["/sessions/week_9/lecture", "/sessions/week_9/"],
+      // },
 
-      {
-        title: "Week 10 - Further Mongo Relations and Dynamic JavaScript",
-        collapsable: true,
-        children: ["/sessions/week_10/lecture", "/sessions/week_10/"],
-      },
+      // {
+      //   title: "Week 10 - Further Mongo Relations and Dynamic JavaScript",
+      //   collapsable: true,
+      //   children: ["/sessions/week_10/lecture", "/sessions/week_10/"],
+      // },
     ],
   },
 };
